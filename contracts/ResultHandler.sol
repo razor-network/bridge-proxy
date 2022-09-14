ac70d022aa796b45e3dcf228a531b47a6f83553c
rajkharvar <rajkharvar1808@gmail.com>
2022-09-14 17:34:23 +0530

    Implemented IDelegator methods in ResultHandler for data feeds

--- contracts/ResultHandler.sol
+++ contracts/ResultHandler.sol
@@ -12,14 +12,19 @@ contract ResultHandler {
     address public constant RESULT_PROXY_ADDRESS =
         0x54EB375F80f6feCA26BaA49A76dc7FB35bd04a03;
 
-    mapping(uint16 => CollectionResult) public collectionResults;
+    struct Collection {
+        uint16 id;
+        int8 power;
+        uint256 result;
+    }
+
+    /// @notice mapping for name of collection in bytes32 -> collectionid
+    mapping(bytes32 => uint16) public collectionIds;
+
+    /// @notice mapping for CollectionID -> Collection Info
+    mapping(uint16 => Collection) public collections;
 
     event DataReceived(bytes32 schainHash, address sender, bytes data);
-
-    struct CollectionResult {
-        uint256 result;
-        int8 power;
-    }
 
     modifier onlyMessageProxy() {
         require(
@@ -49,8 +54,9 @@ contract ResultHandler {
             uint16[] memory ids,
             uint256[] memory results,
             int8[] memory power,
+            bytes32[] memory namesHash,
             uint256 timestamp
-        ) = abi.decode(data, (uint16[], uint256[], int8[], uint256));
+        ) = abi.decode(data, (uint16[], uint256[], int8[], bytes32[], uint256));
 
         updatedCounter++;
 
@@ -58,43 +64,60 @@ contract ResultHandler {
         lastUpdatedTimestamp = timestamp;
 
         for (uint256 i = 0; i < ids.length; i++) {
-            collectionResults[ids[i]].result = results[i];
-            collectionResults[ids[i]].power = power[i];
+            collections[ids[i]].result = results[i];
+            collections[ids[i]].power = power[i];
+            collectionIds[namesHash[i]] = ids[i];
         }
         emit DataReceived(schainHash, sender, data);
         return sender;
     }
 
     /**
-     * @dev Returns all active collection ids, result, power and lastUpdated timestamp
+     * @dev using the hash of collection name, clients can query collection id with respect to its hash
+     * @param _name bytes32 hash of the collection name
+     * @return collection ID
      */
-    function getAllResult()
-        public
-        view
-        returns (
-            uint16[] memory,
-            uint256[] memory,
-            int8[] memory,
-            uint256
-        )
-    {
-        uint256 activeCollectionsLength = activeCollectionIds.length;
+    function getCollectionID(bytes32 _name) public view returns (uint16) {
+        return collectionIds[_name];
+    }
 
-        uint256[] memory results = new uint256[](activeCollectionsLength);
-        int8[] memory power = new int8[](activeCollectionsLength);
-
-        for (uint256 i = 0; i < activeCollectionsLength; i++) {
-            results[i] = collectionResults[activeCollectionIds[i]].result;
-            power[i] = collectionResults[activeCollectionIds[i]].power;
-        }
-
-        return (activeCollectionIds, results, power, lastUpdatedTimestamp);
+    /**
+     * @dev using the hash of collection name, clients can query the result of that collection
+     * @param _name bytes32 hash of the collection name
+     * @return result of the collection and its power
+     */
+    function getResult(bytes32 _name) public view returns (uint256, int8) {
+        uint16 id = collectionIds[_name];
+        return getResultFromID(id);
     }
 
     /**
      * @dev Returns collection result and power with collectionId as parameter
      */
-    function getResult(uint16 _id) public view returns (uint256, int8) {
-        return (collectionResults[_id].result, collectionResults[_id].power);
+    function getResultFromID(uint16 _id) public view returns (uint256, int8) {
+        return (collections[_id].result, collections[_id].power);
+    }
+
+    /**
+     * @return ids of active collections in the oracle
+     */
+    function getActiveCollections() external view returns (uint16[] memory) {
+        return activeCollectionIds;
+    }
+
+    /**
+     * @dev using the collection id, clients can query the status of collection
+     * @param _id collection ID
+     * @return status of the collection
+     */
+    function getCollectionStatus(uint16 _id) public view returns (bool) {
+        bool isActive;
+        for (uint256 i = 0; i < 16; i++) {
+            if (activeCollectionIds[i] == _id) {
+                isActive = true;
+                break;
+            }
+        }
+        return isActive;
     }
 }
