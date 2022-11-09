db2ec1a3e2d9a8174077c922e199926b9d61fc23
rajkharvar <rajkharvar1808@gmail.com>
2022-11-09 12:25:12 +0530

    Update result manager deployment script

--- scripts/deployResultManagerProxy.js
+++ scripts/deployResultManagerProxy.js
@@ -1,8 +1,6 @@
 const hre = require("hardhat");
-const proxySchainV3 = require("../abis/proxySchainv3.json");
 
-const SOURCE_CHAIN = "whispering-turais";
-const SIGNER_ADDRESS = "0x905173B6C0A51925d3C9B619466c623c754Fb7BB";
+const SIGNER_ADDRESS = "0x5eED350E394a2ab53023E8A4D706B4FC5117f401";
 
 async function main() {
   const ResultManagerProxy = await hre.ethers.getContractFactory(
@@ -12,21 +10,7 @@ async function main() {
     SIGNER_ADDRESS,
   ]);
   await instance.deployed();
-
   console.log("ResultManager proxy address: ", instance.address);
-
-  const proxySchainContract = await hre.ethers.getContractAt(
-    proxySchainV3.message_proxy_chain_abi,
-    proxySchainV3.message_proxy_chain_address
-  );
-  console.log("Registering ResultManager proxy contract on Schain");
-  const tx = await await proxySchainContract.registerExtraContract(
-    SOURCE_CHAIN,
-    instance.address
-  );
-  await tx.wait();
-  console.log("tx");
-  console.log(tx);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
