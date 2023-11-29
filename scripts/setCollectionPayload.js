3b4a4d0b17b1aa95c4682f88ebe8896e457300f1
rajkharvar <rajkharvar1808@gmail.com>
2023-11-29 17:57:04 +0530

    chore: update setCollectionPayload script

--- scripts/setCollectionPayload.js
+++ scripts/setCollectionPayload.js
@@ -1,7 +1,8 @@
 const hre = require("hardhat");
-require('dotenv').config();
+require("dotenv").config();
 
-const SIGNER_ADDRESS = process.env.SIGNER_ADDRESS || "0xC68AcC784227DbEaE98Bb6F5aC3C57cCe1aE9B4B";
+const SIGNER_ADDRESS =
+  process.env.SIGNER_ADDRESS || "0xC68AcC784227DbEaE98Bb6F5aC3C57cCe1aE9B4B";
 const COLLECTION_NAME_HASH =
   "0x7404e3d104ea7841c3d9e6fd20adfe99b4ad586bc08d8f3bd3afef894cf184de"; // BTCUSD
 const PAYLOAD =
@@ -9,12 +10,12 @@ const PAYLOAD =
 const FORWARDER_ADDRESS = "0xa26232204879C4370806cA56F821b9fe97eAeF5B"; // This address should be updated depending on the chain script is running on
 const FORWARDER_ABI = require("../abis/Forwarder.json");
 
-
 async function main() {
   const signer = await hre.ethers.getSigner();
-  const forwarder = new ethers.Contract(FORWARDER_ADDRESS, FORWARDER_ABI, signer);
-console.log(
-    `[Forwarder] Contract: ${FORWARDER_ADDRESS} Setting collection name hash: ${COLLECTION_NAME_HASH} with payload ${PAYLOAD}`
+  const Forwarder = await hre.ethers.getContractFactory("Forwarder");
+  const forwarder = Forwarder.attach(FORWARDER_ADDRESS);
+  console.log(
+    `[Forwarder] Contract: ${forwarder.address} Setting collection name hash: ${COLLECTION_NAME_HASH} with payload ${PAYLOAD}`
   );
   const tx1 = await forwarder.setCollectionPayload(
     COLLECTION_NAME_HASH,
